3d3ad88824da14b7cc6c745603bab7c9fe3b6190
Daniel Martelly <dmartelly@pocketgems.com>
2015-03-09 19:25:49 -0400

    Player moves around using forces

---
+++ Sound Game/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		rb2d.AddForce(new Vector2(horizontal*10, vertical*10));
+	}
+}
