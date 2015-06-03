1c014de6a566b94ef31b5d00eb7569f353e74186
Daniel Martelly <dmartelly@pocketgems.com>
2015-06-03 04:32:37 -0400

    Trying a new type of sound wave using multiple small segments which expand

---
+++ Sound Game/Assets/WavePiece.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WavePiece : MonoBehaviour {
+
+	public Vector2 startPosition;
+	public Vector2 speed;
+	public float maxTravelDistance;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		speed = new Vector2 (.05f, .05f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 prev = transform.position;
+		transform.position = new Vector3(prev.x+speed.x, prev.y+speed.y, prev.z);
+	}
+}
